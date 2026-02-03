9705ba9560945882ab56a0f99a8fce46c9c3f6b9
Harsh Prajapati <harshprajapati8347@gmail.com>
2026-02-03 22:52:25 +0530

    feat - gtm snippet imstall

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { AnimatePresence } from 'framer-motion'
 import theme from 'config/theme'
 import FavIconProvider from 'components/Misc/FavIconProvider'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 function HPSite({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <AnimatePresence exitBeforeEnter>
       <ChakraProvider theme={theme}>
+        <GoogleTagManager gtmId="GTM-WVR8KDZ" />
         <FavIconProvider>
           <Component {...pageProps} />
         </FavIconProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
@@ -9,7 +10,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           ></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;1,100;1,300;1,400;1,500;1,600&display=swap"
